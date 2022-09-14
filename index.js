5daa66f0ed56d543c3887ee723dbc52718a140c7
Destinee Saunders <dsaunders290@gmail.com>
2022-09-14 16:42:18 -0400

    commit tree cf47ac733705098448ba11501cb173221452dac5

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    let records = record.find(({result}) => result === "W")
+    if (records ===undefined){
+        return undefined
+    } else {
+    return records.year
+}
+} 
+//console.log(record.find(superbowlWin))
